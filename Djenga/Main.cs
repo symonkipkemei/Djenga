8fe0a74bb111a0de562d3a9dd4b8047f506bb57f
Symon Kipkemei <symonkipkemei@gmail.com>
2023-11-27 19:48:27 +0300

    feat: External command and user selection view integration logic

--- Djenga/Main.cs
+++ Djenga/Main.cs
@@ -1,1 +1,32 @@
 ﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.Windows.Interop;
+using Autodesk.Revit.Attributes;
+
+
+namespace Djenga
+{
+
+
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        { 
+            // Show setting table
+
+            // 
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
