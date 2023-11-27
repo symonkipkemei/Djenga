8196493ecde96ac7e45366366b1612221fc049c7
Symon Kipkemei <symonkipkemei@gmail.com>
2023-11-27 19:50:26 +0300

    feat: Add selection logic

--- Djenga/Model/Logic.cs
+++ Djenga/Model/Logic.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.Windows.Controls;
 
 namespace Djenga.Model
 {
-    internal class Logic
+    public class Logic
     {
+        public IList<Element> SelectMultipleWalls(UIDocument uidoc,Document doc)
+        {
+            IList<Element> walls = new List<Element>();
+            IList<Reference> references = uidoc.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element);
+            foreach (Reference reference in references)
+            {
+                Element wall = doc.GetElement(reference);
+                walls.Add(wall);
+            }
+
+            return walls;
+        }
+
+        public IList<Element> SelectAllWalls(Document doc)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            IList<Element> walls = collector.OfCategory(BuiltInCategory.OST_Walls).WhereElementIsNotElementType().ToElements();
+            return walls;
+        }
+
+        
+        
     }
 }
+
+
