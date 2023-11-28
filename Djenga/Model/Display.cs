f70fffbe58d061777bd49e3d637a9d61ede1f407
Symon Kipkemei <symonkipkemei@gmail.com>
2023-11-28 23:17:56 +0300

    feat: Add display data model

---
+++ Djenga/Model/Display.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Djenga.Model
+{
+    public class Display
+    {
+        public string Description { get; set; }
+        public string Unit { get; set; }
+
+        public double Quantity { get; set; }
+
+        public double Rate { get; set; }
+
+        public double Amount { get; set; }
+    }
+}
