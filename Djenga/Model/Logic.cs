db9214bfdb397df4c3330036a5aec27e872ab515
Symon Kipkemei <symonkipkemei@gmail.com>
2023-12-02 00:38:18 +0300

    chore: update on model folder structure and dependencies

--- Djenga/Model/Logic.cs
+++ Djenga/Model/Logic.cs
@@ -7,12 +7,16 @@ using System.Threading.Tasks;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.DB;
 using System.Windows.Controls;
+using System.Collections.ObjectModel;
 
 namespace Djenga.Model
 {
     public class Logic
     {
-        public IList<Element> SelectMultipleWalls(UIDocument uidoc,Document doc)
+
+        public ObservableCollection<Display> Items { get; set; }
+
+        public IList<Element> SelectMultipleWalls(UIDocument uidoc, Document doc)
         {
             IList<Element> walls = new List<Element>();
             IList<Reference> references = uidoc.Selection.PickObjects(Autodesk.Revit.UI.Selection.ObjectType.Element);
@@ -32,8 +36,94 @@ namespace Djenga.Model
             return walls;
         }
 
-        
-        
+
+        public void AbstractWallMaterialData(IList<Element> walls, double mortarThickness, double masonryHeight, double masonryLength, double masonryWidth)
+        {
+            Items = new ObservableCollection<Display>();
+
+            // For every wall
+            foreach (Element wall in walls)
+            {
+                // abstract wall data
+                double length = wall.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
+                double height = wall.get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsDouble();
+                double width = 0.656168; // to be fixed , set to 200mm thick
+
+                // Intantiate ingredients for creating wall courses
+                Course firstCourse = new Course(width, length, mortarThickness, masonryHeight, masonryLength, masonryWidth);
+                Course secondCourse = new Course(width, length, mortarThickness, masonryHeight, masonryLength, masonryWidth);
+                HoopIronStrip hoopiron = new HoopIronStrip(length);
+                DpcStrip dpcStrip = new DpcStrip(width, length);
+
+                // Create  courses by adding individual elements i.e blocks and mortar
+                firstCourse.AddCourseElements(true);
+                secondCourse.AddCourseElements(false);
+
+                // Intantiate ingredients for creating an abstract wall
+                Ukuta ukuta = new Ukuta(firstCourse, secondCourse, hoopiron, dpcStrip);
+                ukuta.AddCourses(height);
+
+                // stones
+                Items.Add(new Display
+                {
+                    Description = ukuta.FirstCourse.FullBlock.Name,
+                    Unit = ukuta.FirstCourse.FullBlock.Unit,
+                    Quantity = ukuta.TotalBlocks(),
+                    Rate = ukuta.FirstCourse.FullBlock.Rate,
+                    Amount = ukuta.FirstCourse.FullBlock.Amount()
+                });
+
+                // cement
+                Items.Add(new Display
+                {
+                    Description = ukuta.FirstCourse.HorizontalJoint.Mortar.Cement.Name,
+                    Unit = ukuta.FirstCourse.HorizontalJoint.Mortar.Cement.Unit,
+                    Quantity = ukuta.TotalCementWeight(),
+                    Rate = ukuta.FirstCourse.HorizontalJoint.Mortar.Cement.Rate,
+                    Amount = ukuta.FirstCourse.HorizontalJoint.Mortar.Cement.Amount(),
+
+                });
+
+
+                // sand
+                Items.Add(new Display
+                {
+                    Description = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Name,
+                    Unit = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Unit,
+                    Quantity = ukuta.TotalSandWeight(),
+                    Rate = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Rate,
+                    Amount = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Amount(),
+
+                });
+
+
+                // Hoop Iron
+
+                Items.Add(new Display
+                {
+                    Description = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Name,
+                    Unit = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Unit,
+                    Quantity = ukuta.TotalSandWeight(),
+                    Rate = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Rate,
+                    Amount = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Amount(),
+
+                });
+
+
+
+                // DPC
+                Items.Add(new Display
+                {
+                    Description = ukuta.DpcStrip.Name,
+                    Unit = ukuta.DpcStrip.Unit,
+                    Quantity = ukuta.TotalDpcRolls(),
+                    Rate = ukuta.DpcStrip.Rate,
+                    Amount = ukuta.DpcStrip.Amount(),
+
+                });
+            }
+
+        }
     }
 }
 
--- Djenga/View/UserSelection.xaml.cs
+++ Djenga/View/UserSelection.xaml.cs
@@ -27,7 +27,7 @@ namespace Djenga.View
 
         // store external command data reference
         ExternalCommandData CommandData { get; }
-        public ObservableCollection<Display> Items { get; set; }
+       
 
         public UserSelection(ExternalCommandData commandData)
         {
@@ -64,7 +64,7 @@ namespace Djenga.View
             // Revit parameters
             UIDocument uidoc = CommandData.Application.ActiveUIDocument;
             Document doc = uidoc.Document;
-            Items = new ObservableCollection<Display>();
+            
 
             // close window before selecting walls
             this.Close();
@@ -73,91 +73,11 @@ namespace Djenga.View
             Logic logic = new Logic();
             IList<Element> walls = logic.SelectMultipleWalls(uidoc, doc);
 
-
-            // For every wall
-            foreach (Element wall in walls)
-            {
-                // abstract wall data
-                double length = wall.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
-                double height = wall.get_Parameter(BuiltInParameter.WALL_USER_HEIGHT_PARAM).AsDouble();
-                double width = 0.656168; // to be fixed , set to 200mm thick
-
-                // Intantiate ingredients for creating wall courses
-                Course firstCourse = new Course(width, length, mortarThickness, masonryHeight, masonryLength, masonryWidth);
-                Course secondCourse = new Course(width, length, mortarThickness, masonryHeight, masonryLength, masonryWidth);
-                HoopIron hoopiron = new HoopIron(length);
-                DpcStrip dpcStrip = new DpcStrip(width, length);
-
-                // Create  courses by adding individual elements i.e blocks and mortar
-                firstCourse.AddCourseElements(true);
-                secondCourse.AddCourseElements(false);
-
-                // Intantiate ingredients for creating an abstract wall
-                Ukuta ukuta = new Ukuta(firstCourse, secondCourse, hoopiron, dpcStrip);
-                ukuta.AddCourses(height);
-
-                // stones
-                Items.Add(new Display
-                {
-                    Description = ukuta.FirstCourse.FullBlock.Name,
-                    Unit = ukuta.FirstCourse.FullBlock.Unit,
-                    Quantity = ukuta.TotalBlocks(),
-                    Rate = ukuta.FirstCourse.FullBlock.Rate,
-                    Amount = ukuta.FirstCourse.FullBlock.Amount()
-                });
-
-                // cement
-                Items.Add(new Display
-                {
-                    Description = ukuta.FirstCourse.HorizontalJoint.Mortar.Cement.Name,
-                    Unit = ukuta.FirstCourse.HorizontalJoint.Mortar.Cement.Unit,
-                    Quantity = ukuta.TotalCementWeight(),
-                    Rate = ukuta.FirstCourse.HorizontalJoint.Mortar.Cement.Rate,
-                    Amount = ukuta.FirstCourse.HorizontalJoint.Mortar.Cement.Amount(),
-
-                });
+            // Abstract material data from walls and store it on display
+            logic.AbstractWallMaterialData(walls, mortarThickness, masonryHeight, masonryLength, masonryWidth);
 
 
-                // sand
-                Items.Add(new Display
-                {
-                    Description = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Name,
-                    Unit = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Unit,
-                    Quantity = ukuta.TotalSandWeight(),
-                    Rate = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Rate,
-                    Amount = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Amount(),
-
-                });
-
-
-                // Hoop Iron
-
-                Items.Add(new Display
-                {
-                    Description = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Name,
-                    Unit = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Unit,
-                    Quantity = ukuta.TotalSandWeight(),
-                    Rate = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Rate,
-                    Amount = ukuta.FirstCourse.HorizontalJoint.Mortar.Sand.Amount(),
-
-                });
-
-
-
-                // DPC
-                Items.Add(new Display
-                {
-                    Description = ukuta.DpcStrip.Name,
-                    Unit = ukuta.DpcStrip.Unit,
-                    Quantity = ukuta.TotalDpcRolls(),
-                    Rate = ukuta.DpcStrip.Rate,
-                    Amount = ukuta.DpcStrip.Amount(),
-
-                });
-
-
-            }
-            UserDisplay displayForm = new UserDisplay(Items);
+            UserDisplay displayForm = new UserDisplay(logic.Items);
             displayForm.ShowDialog();
 
         }
